cf6758a0cda33103cf800eaafed03e283f6db8f1
Timofey Volkov <timofey.volkov@gmail.com>
2021-04-25 12:24:33 +0300

    create file

--- src/main/java/com/smartbear/controller/FileController.java
+++ src/main/java/com/smartbear/controller/FileController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Pattern;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequiredArgsConstructor
 @Validated
@@ -18,19 +20,14 @@ public class FileController {
 
     private final FileService fileService;
 
-    @PostMapping("/file")
+    @PostMapping(value = "/file", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<CreateFileResponse> createFile(@RequestBody CreateFileRequest createFileRequest){
         return ResponseEntity.ok(fileService.createFile(createFileRequest));
     }
 
-    @GetMapping("/files/{tag_search_query}/{page}")
+    @GetMapping(value = "/files/{tag_search_query}/{page}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<SearchFilesResponse> searchFiles(@PathVariable("tag_search_query") @Pattern(regexp = "[+\\-][a-zA-Z0-9]+") String tagSearchQuery,
                                                            @PathVariable("page") @Pattern(regexp = "\\d") int page){
         return ResponseEntity.ok(fileService.search(tagSearchQuery, page));
-    }
-
-    @GetMapping("/test")
-    public String test(){
-        return "test";
     }
 }

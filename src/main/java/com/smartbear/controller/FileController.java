f6e619517184e3d650c0ed7f85c51fe5456ff3bf
Timofey Volkov <timofey.volkov@gmail.com>
2021-04-26 10:16:14 +0300

    cleanup

--- src/main/java/com/smartbear/controller/FileController.java
+++ src/main/java/com/smartbear/controller/FileController.java
@@ -7,14 +7,12 @@ import com.smartbear.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
-@Validated
 public class FileController {
 
     private final FileService fileService;
